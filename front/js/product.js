2e5f785fd90fe6b2552c417aa0a9bc06e1269751
Yohan <yohan.battesti@gmail.com>
2022-04-28 16:05:22 +0100

    boucle pour les couleurs , ajout de localstorage

--- front/js/product.js
+++ front/js/product.js
@@ -1,12 +1,16 @@
+// récuperation de l'id
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
 
 console.log(id)
 
+// recuperation des articles par l'api
 fetch (`http://localhost:3000/api/products/${id}`)
     .then(data => {
         return data.json()
     })
+
+    // récuperation des information des articles
     .then(item =>{
    
         let itemImg = document.querySelector(".item__img");
@@ -28,10 +32,46 @@ fetch (`http://localhost:3000/api/products/${id}`)
             itemDescription.innerHTML += `
         ${item.description}
         `;
-       
-        console.log(item.name)
-       
-    });
 
-   
+        let itemColor = document.getElementById("colors");
+        item.colors.forEach((color) => {
+                 itemColor.innerHTML += `
+         <option> ${color} </option>
+        `;
+        });
+         
+        //console.log(item)
+       
+    // au clic on ajoute dans le panier
+
+    let btnPanier = document.getElementById("addToCart")
+    btnPanier.addEventListener("click" , ()=> {
+        console.log("ok")
+    },false);
+
+    // si j'ai un panier (localstorage) je récupere les info du panier pour ajouter des nouveaux produits
+
+    cardProduits = JSON.parse(localStorage.getItem('panier'));
+
+    // creation de tableau dans local storage
+
+    // let myProduit = cardProduits;
+    myProduit = {
+        id : item._id,
+        name : item.name,
+        imageUrl : item.imageUrl,
+        description : item.description,
+        price : item.price
+    };
+
+    // trnasformation du tableau en JSON
+    myProduit = JSON.stringify(myProduit);
+
+    // mettre les information qu'on a besoin
+    localStorage.setItem('panier',myProduit)
+
+    // fin du click
+    console.log(localStorage.getItem('panier'))
+
+ });
       
