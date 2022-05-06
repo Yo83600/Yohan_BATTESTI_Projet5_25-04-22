35518aaeb84be961ee484740c4063aec6ae827f4
Yohan <yohan.battesti@gmail.com>
2022-05-06 11:27:49 +0100

    suppression du symbole € (correction syntaxe)

--- front/js/index.js
+++ front/js/index.js
@@ -1,51 +1,50 @@
 // recupération des produits par l'url
-fetch ("http://localhost:3000/api/products")
+fetch("http://localhost:3000/api/products")
     .then(data => {
         return data.json()
-    }).then(products =>{
+    }).then(products => {
 
-    // variable pour recuperer le nom de la section en HTML
-    let HTML = document.getElementById("items")
+        // variable pour recuperer le nom de la section en HTML
+        let HTML = document.getElementById("items")
 
-    //let myHTML = ""
+        //let myHTML = ""
 
-    // création de la card produit 
-    products.forEach(product => {
+        // création de la card produit 
+        products.forEach(product => {
 
-      let card = document.createElement("a");
-      card.href = `./product.html?id=${product._id}`
+            let card = document.createElement("a");
+            card.href = `./product.html?id=${product._id}`
 
-      let cardArticle = document.createElement("article");
+            let cardArticle = document.createElement("article");
 
-      let cardImg = document.createElement("img");
-      cardImg.src = product.imageUrl;
-      cardImg.alt = product.altTxt;
-      cardArticle.appendChild(cardImg)
+            let cardImg = document.createElement("img");
+            cardImg.src = product.imageUrl;
+            cardImg.alt = product.altTxt;
+            cardArticle.appendChild(cardImg)
 
-      let cardTitle = document.createElement("h3");
-      cardTitle.textContent = `${product.name}`
+            let cardTitle = document.createElement("h3");
+            cardTitle.textContent = `${product.name}`
 
-      cardArticle.appendChild(cardTitle)
+            cardArticle.appendChild(cardTitle)
 
-      let cardDescription = document.createElement("p");
-      cardDescription.textContent = `${product.description} €`
+            let cardDescription = document.createElement("p");
+            cardDescription.textContent = `${product.description}`
 
-      cardArticle.appendChild(cardDescription)
+            cardArticle.appendChild(cardDescription)
 
-      card.appendChild(cardArticle)
-     
-      HTML.appendChild(card)
+            card.appendChild(cardArticle)
+
+            HTML.appendChild(card)
 
 
-        //  myHTML += `<a href="./product.html?id=${product._id}">
-        //   <article>
-        //     <img src="${product.imageUrl}" alt="${product.name}">
-        //     <h3 class="productName">${product.name}</h3>
-        //     <p class="productDescription">${product.price} €</p>
-        //   </article>
-        // </a>`
-    });
+            //  myHTML += `<a href="./product.html?id=${product._id}">
+            //   <article>
+            //     <img src="${product.imageUrl}" alt="${product.name}">
+            //     <h3 class="productName">${product.name}</h3>
+            //     <p class="productDescription">${product.price} €</p>
+            //   </article>
+            // </a>`
+        });
 
-    //HTML.innerHTML = myHTML
-})
-
+        //HTML.innerHTML = myHTML
+    })
